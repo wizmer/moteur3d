8f49f9dcb99c09e093cc78584fd72dcadb414c69
ben.coste <ben.coste@631af564-f1f8-951a-4761-5774ceaa8e83>
2012-03-02 12:25:05 +0000

    add geometry

--- ProjPoint.cpp
+++ ProjPoint.cpp
@@ -5,6 +5,11 @@
 ProjPoint::ProjPoint(Point* fPoint):m_point(fPoint){}
 
 void ProjPoint::Update(Camera* cam){
+  // double GlobalX = 0, GlobalY = 0, GlobalZ = 0;
+  // for(int i = 0;i<3;i++){
+  //     GlobalX += 
+  // }
+
   Phi = atan((m_point->GetX() - cam->GetX())/(m_point->GetY() - cam->GetY())) - cam->GetPhi();
   Tau = asin(m_point->GetZ() - cam->GetZ())/sqrt(pow(m_point->GetX() - cam->GetX(),2) +pow(m_point->GetY() - cam->GetY(),2)) - cam->GetTau();
    if(m_point->GetY() -cam->GetY() < 0) Phi += PI;
